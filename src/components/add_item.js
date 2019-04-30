49fbf7e2e602024ac810a0f4661d364bf7f86ade
Justen Quirante <jcquirante@gmail.com>
2019-04-29 22:00:13 -0700

    Add Item in progress

---
+++ src/components/add_item.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AddItem extends Component {
+    render() {
+        return(
+            <h1>List Item</h1>
+        )
+        
+    }
+}
+
+export default AddItem;
