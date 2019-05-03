73ebd9ace099064765251033a3855a04e6b56d11
Justen Quirante <jcquirante@gmail.com>
2019-05-02 22:37:44 -0700

    Moved list state to app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,39 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
-import React from 'react';
+import React, { Component } from 'react';
 import List from './list';
 import AddItem from './add_item';
+import dummyList from '../data/to_do_list';
 
 
-const App = () => (
-    <div className="container">
-        <h1 className="center">To Do List</h1>
-        <AddItem/>
-        <List/>
-    </div>
-);
+class App extends Component { 
+
+    state = {
+        list: [],
+    }
+
+    getListData() {
+        this.setState({
+            list: dummyList,
+        })
+    }
+
+    componentDidMount() {
+        this.getListData();
+    }
+    render() {
+
+        const { list } = this.state;
+
+        return(
+            <div className="container">
+                <h1 className="center">To Do List</h1>
+                <AddItem/>
+                <List toDos={list}/>
+            </div>
+        )
+    }
+    
+}
 
 export default App;
